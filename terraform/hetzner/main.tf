1ca2495efde10a500556e7d0a100025c34ec47ec
Leonhard Nobach <leo@nobach.net>
2020-05-28 13:24:49 +0200

    terraform fmt

--- terraform/hetzner/main.tf
+++ terraform/hetzner/main.tf
@@ -16,13 +16,13 @@ resource "hcloud_server" "vb" {
   server_type = "cpx11"
   ssh_keys    = data.hcloud_ssh_keys.my_keys.ssh_keys.*.id
   user_data = templatefile("${path.module}/../../cloud-config.template.yaml", {
-    cc_server_ipv4      = hcloud_floating_ip.vb_v4.ip_address
-    cc_server_ipv6      = cidrhost(hcloud_floating_ip.vb_v6.ip_network, 1)
-    cc_frontend_domain  = var.frontend_domain
-    cc_letsencrypt_mail = var.letsencrypt_mail
-    cc_nc_endpoint         = var.nc_endpoint
-    cc_nc_sharedsecret     = var.nc_sharedsecret
-    cc_turn_sharedsecret   = var.turn_sharedsecret
+    cc_server_ipv4       = hcloud_floating_ip.vb_v4.ip_address
+    cc_server_ipv6       = cidrhost(hcloud_floating_ip.vb_v6.ip_network, 1)
+    cc_frontend_domain   = var.frontend_domain
+    cc_letsencrypt_mail  = var.letsencrypt_mail
+    cc_nc_endpoint       = var.nc_endpoint
+    cc_nc_sharedsecret   = var.nc_sharedsecret
+    cc_turn_sharedsecret = var.turn_sharedsecret
   })
 }
 
@@ -52,12 +52,12 @@ resource "hcloud_floating_ip_assignment" "vb_v6" {
 
 resource "hcloud_rdns" "float_v4" {
   floating_ip_id = hcloud_floating_ip.vb_v4.id
-  ip_address = hcloud_floating_ip.vb_v4.ip_address
-  dns_ptr = var.frontend_domain
+  ip_address     = hcloud_floating_ip.vb_v4.ip_address
+  dns_ptr        = var.frontend_domain
 }
 
 resource "hcloud_rdns" "float_v6" {
   floating_ip_id = hcloud_floating_ip.vb_v6.id
-  ip_address = cidrhost(hcloud_floating_ip.vb_v6.ip_network, 1)
-  dns_ptr = var.frontend_domain
+  ip_address     = cidrhost(hcloud_floating_ip.vb_v6.ip_network, 1)
+  dns_ptr        = var.frontend_domain
 }
